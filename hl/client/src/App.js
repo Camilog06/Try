888a87de1d2029858d4fc1ab4e295bfc4b88ab90
camilogoez06 <camilogoez06@gmail.com>
2024-04-29 16:22:30 -0500

    add client/server

--- hl/client/src/App.js
+++ hl/client/src/App.js
@@ -7,16 +7,24 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          YUYUFEST
         </p>
+        <h1>
+          El Éxito consiste en persivir día a dia lo que queremos
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://instagram.com/yuyufest_?igsh=eWRrZnp6bXYzcGRx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visitanos y conoce nuestros nuevos eventos
         </a>
+        <div>
+          <form>
+            
+          </form>
+        </div>
       </header>
     </div>
   );
